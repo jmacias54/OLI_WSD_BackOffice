50c9bd6cffb32412e5c4995b7a33a76dbf5c27db
Hitss <jesusmaciasbenitez@gmail.com>
2017-11-22 17:24:38 -0600

    Se agrega funcionalidad al controller nnota

--- src/mx/com/amx/unotv/oli/wsd/backoffice/controller/NNotaController.java
+++ src/mx/com/amx/unotv/oli/wsd/backoffice/controller/NNotaController.java
@@ -3,11 +3,19 @@
  */
 package mx.com.amx.unotv.oli.wsd.backoffice.controller;
 
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import mx.com.amx.unotv.oli.wsd.backoffice.controller.exception.ControllerException;
 import mx.com.amx.unotv.oli.wsd.backoffice.dao.NNotaDAO;
+import mx.com.amx.unotv.oli.wsd.backoffice.model.NNota;
+import mx.com.amx.unotv.oli.wsd.backoffice.response.ListResponse;
 
 
 /**
@@ -24,5 +32,108 @@ public class NNotaController {
 	
 	@Autowired
 	NNotaDAO nNotaDAO;
+	
+	
+	@RequestMapping(value = "/", method = RequestMethod.POST, headers = "Accept=application/json; charset=utf-8")
+	@ResponseBody
+	public ListResponse<NNota> findAll() throws ControllerException{
+		
+		logger.info("--- findAll [ NNotaController ]---- ");
 
+		List<NNota> lista = null;
+		ListResponse<NNota> response = null;
+
+		try {
+
+			lista = nNotaDAO.findAll();
+
+			if (lista != null && !lista.isEmpty()) {
+				response = new ListResponse<NNota>();
+				response.setLista(lista);
+			}
+
+		} catch (Exception e) {
+			logger.error(" -- Error  findAll [ NNotaController ]:", e);
+			throw new ControllerException(e.getMessage());
+		}
+
+		return response;
+	}
+
+	@RequestMapping(value = "/categoria/{idCategoria}", method = RequestMethod.POST, headers = "Accept=application/json; charset=utf-8")
+	@ResponseBody
+	public ListResponse<NNota> findByIdCategoria(@PathVariable String idCategoria) throws ControllerException {
+		
+		logger.info("--- findByIdCategoria [ NNotaController ]---- ");
+
+		List<NNota> lista = null;
+		ListResponse<NNota> response = null;
+
+		try {
+
+			lista = nNotaDAO.findByIdCategoria(idCategoria);
+
+			if (lista != null && !lista.isEmpty()) {
+				response = new ListResponse<NNota>();
+				response.setLista(lista);
+			}
+
+		} catch (Exception e) {
+			logger.error(" -- Error  findByIdCategoria [ NNotaController ]:", e);
+			throw new ControllerException(e.getMessage());
+		}
+
+		return response;
+	}
+	
+	@RequestMapping(value = "/tipoNota/{tipoNota}", method = RequestMethod.POST, headers = "Accept=application/json; charset=utf-8")
+	@ResponseBody
+	public ListResponse<NNota> findByTipoNota(@PathVariable String tipoNota) throws ControllerException {
+		logger.info("--- findByTipoNota [ NNotaController ]---- ");
+
+		List<NNota> lista = null;
+		ListResponse<NNota> response = null;
+
+		try {
+
+			lista = nNotaDAO.findByTipoNota(tipoNota);
+
+			if (lista != null && !lista.isEmpty()) {
+				response = new ListResponse<NNota>();
+				response.setLista(lista);
+			}
+
+		} catch (Exception e) {
+			logger.error(" -- Error  findByTipoNota [ NNotaController ]:", e);
+			throw new ControllerException(e.getMessage());
+		}
+
+		return response;
+	}
+	
+	@RequestMapping(value = "/classVideo/{idClassVideo}", method = RequestMethod.POST, headers = "Accept=application/json; charset=utf-8")
+	@ResponseBody
+	public ListResponse<NNota> findByIdClassVideo(@PathVariable String idClassVideo) throws ControllerException {
+		logger.info("--- findByIdClassVideo [ NNotaController ]---- ");
+
+		List<NNota> lista = null;
+		ListResponse<NNota> response = null;
+
+		try {
+
+			lista = nNotaDAO.findByIdClassVideo(idClassVideo);
+
+			if (lista != null && !lista.isEmpty()) {
+				response = new ListResponse<NNota>();
+				response.setLista(lista);
+			}
+
+		} catch (Exception e) {
+			logger.error(" -- Error  findByIdClassVideo [ NNotaController ]:", e);
+			throw new ControllerException(e.getMessage());
+		}
+
+		return response;
+		
+	}
 }
--- src/mx/com/amx/unotv/oli/wsd/backoffice/dao/NNotaDAO.java
+++ src/mx/com/amx/unotv/oli/wsd/backoffice/dao/NNotaDAO.java
@@ -3,8 +3,7 @@
  */
 package mx.com.amx.unotv.oli.wsd.backoffice.dao;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Collections;
 import java.util.List;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,23 +13,18 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import mx.com.amx.unotv.oli.wsd.backoffice.dao.exception.NNotaDAOException;
 import mx.com.amx.unotv.oli.wsd.backoffice.model.NNota;
 
-
 /**
  * @author Jesus A. Macias Benitez
  *
  */
 public class NNotaDAO {
 	private static Logger logger = Logger.getLogger(NNotaDAO.class);
-	
-	
+
 	/** The jdbc template. */
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-	
-	SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	SimpleDateFormat dateFormat_2 = new SimpleDateFormat("yyyy-MM-dd");
 
-	
+
 	public List<NNota> findAll() throws NNotaDAOException {
 		List<NNota> lista = null;
 
@@ -49,37 +43,20 @@ public class NNotaDAO {
 
 		}
 
+		if (lista.isEmpty() || lista == null) {
+			return Collections.emptyList();
+		}
+
 		return lista;
 	}
 
-	
-	public void delete(String id) throws NNotaDAOException {
-		logger.info("--- Delete  [HNotaDAO] ---- ");
-
-		StringBuilder query = new StringBuilder();
-		query.append(" DELETE FROM OLI_MX_N_NOTA WHERE FC_ID_CONTENIDO ='" + id + "'");
-		try {
-
-			jdbcTemplate.execute(query.toString());
-
-		} catch (Exception e) {
-
-			logger.error(" Error Delete  [ NNotaDAO ] ", e);
-
-			throw new NNotaDAOException(e.getMessage());
-
-		}
-		
-	}
-
-	
-	public NNota findById(String id) throws NNotaDAOException {
-		logger.info("--- findById  [NNotaDAO] ---- ");
+	public List<NNota> findByIdCategoria(String idCategoria) throws NNotaDAOException {
+		logger.info("--- findByIdCategoria  [NNotaDAO] ---- ");
 
 		List<NNota> lista = null;
 
 		StringBuilder query = new StringBuilder();
-		query.append(" SELECT * FROM OLI_MX_N_NOTA WHERE FC_ID_CONTENIDO ='" + id + "'");
+		query.append(" SELECT * FROM OLI_MX_N_NOTA WHERE FC_ID_CATEGORIA ='" + idCategoria + "'");
 
 		try {
 
@@ -87,101 +64,71 @@ public class NNotaDAO {
 
 		} catch (Exception e) {
 
-			logger.error(" Error findById [ NNotaDAO ] ", e);
+			logger.error(" Error findByIdCategoria [ NNotaDAO ] ", e);
 
 			throw new NNotaDAOException(e.getMessage());
 
 		}
 
 		if (lista.isEmpty() || lista == null) {
-			return null;
+			return Collections.emptyList();
 		}
 
-		return lista.get(0);
+		return lista;
 	}
 
+	public List<NNota> findByTipoNota(String tipoNota) throws NNotaDAOException {
+		logger.info("--- findByTipoNota  [NNotaDAO] ---- ");
 
-	public int update(NNota nota) throws NNotaDAOException {
-		int rows = 0;
+		List<NNota> lista = null;
+
+		StringBuilder query = new StringBuilder();
+		query.append(" SELECT * FROM OLI_MX_N_NOTA WHERE FC_TIPO_NOTA ='" + tipoNota + "'");
 
 		try {
-			rows = jdbcTemplate.update(" UPDATE oli_mx_n_nota SET  "+
-					" FC_ID_CATEGORIA  = ? , "+
-					" FC_TIPO_NOTA  = ? , "+
-					" FC_ID_CLASS_VIDEO  = ? , "+
-					" FC_TITULO  = ? , "+
-					" FC_DESCRIPCION  = ? , "+
-					" FC_FRIENDLY_URL  = ? , "+
-					" FC_AUTOR  = ? , "+
-					" FC_URL_AUTOR  = ? , "+
-					" FC_IMAGEN  = ? , "+
-					" FC_PIE_IMAGEN  = ? , "+
-					" CL_GALERIA  = ? , "+
-					" CL_RTF_CONTENIDO  = ? , "+
-					" FC_ID_YOUTUBE  = ? , "+
-					" FC_ID_CONTENT_OOYALA  = ? , "+
-					" FC_ID_PLAYER_OOYALA  = ? , "+
-					" FC_KEYWORDS  = ? , "+
-					" FD_FECHA_MODIFICACION  = ? , "+
-					" FI_BAN_OTROS  = ?  WHERE FC_ID_CONTENIDO = ? "
-					,nota.getFcIdCategoria(),nota.getFcTipoNota(),nota.getFcIdClassVideo()
-					,nota.getFcTitulo(),nota.getFcDescripcion(),nota.getFcFriendlyUrl()
-					,nota.getFcAutor(),nota.getFcUrlAutor(),nota.getFcImagen()
-					,nota.getFcPieImagen(),nota.getClGaleria(),nota.getClRtfContenido()
-					,nota.getFcIdYoutube(),nota.getFcIdContentOoyala(),nota.getFcIdPlayerOoyala()
-					,nota.getFcKeywords(),dateFormat.format(new Date()) ,nota.getFiBanOtros(),nota.getFcIdContenido());
+
+			lista = jdbcTemplate.query(query.toString(), new BeanPropertyRowMapper<NNota>(NNota.class));
 
 		} catch (Exception e) {
 
-			logger.error(" Error update [ NNotaDAO ] ", e);
+			logger.error(" Error findByTipoNota [ NNotaDAO ] ", e);
 
 			throw new NNotaDAOException(e.getMessage());
 
 		}
 
-		return rows;
+		if (lista.isEmpty() || lista == null) {
+			return Collections.emptyList();
+		}
+
+		return lista;
 	}
 
-	public int insert(NNota nota) throws NNotaDAOException {
-		int rows = 0;
+	public List<NNota> findByIdClassVideo(String idClassVideo) throws NNotaDAOException {
+		logger.info("--- findByIdClassVideo  [NNotaDAO] ---- ");
+
+		List<NNota> lista = null;
+
+		StringBuilder query = new StringBuilder();
+		query.append(" SELECT * FROM OLI_MX_N_NOTA WHERE FC_ID_CLASS_VIDEO ='" + idClassVideo + "'");
 
 		try {
-			rows = jdbcTemplate.update(" INSERT INTO oli_mx_n_nota ( FC_ID_CONTENIDO, "+
-					" FC_ID_CATEGORIA, "+
-					" FC_TIPO_NOTA, "+
-					" FC_ID_CLASS_VIDEO, "+
-					" FC_TITULO, "+
-					" FC_DESCRIPCION, "+
-					" FC_FRIENDLY_URL, "+
-					" FC_AUTOR, "+
-					" FC_URL_AUTOR, "+
-					" FC_IMAGEN, "+
-					" FC_PIE_IMAGEN, "+
-					" CL_GALERIA, "+
-					" CL_RTF_CONTENIDO, "+
-					" FC_ID_YOUTUBE, "+
-					" FC_ID_CONTENT_OOYALA, "+
-					" FC_ID_PLAYER_OOYALA, "+
-					" FC_KEYWORDS, "+
-					" FD_FECHA_PUBLICACION, "+
-					" FD_FECHA_MODIFICACION, "+
-					" FI_BAN_OTROS) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) "
-					, nota.getFcIdContenido(),nota.getFcIdCategoria(),nota.getFcTipoNota()
-					, nota.getFcIdClassVideo(),nota.getFcTitulo() , nota.getFcDescripcion()
-					, nota.getFcFriendlyUrl(),nota.getFcAutor(),nota.getFcUrlAutor()
-					, nota.getFcImagen(), nota.getFcPieImagen(),nota.getClGaleria()
-					, nota.getClRtfContenido(), nota.getFcIdYoutube(), nota.getFcIdContentOoyala()
-					, nota.getFcIdPlayerOoyala(),nota.getFcKeywords(),dateFormat.format(new Date()),
-					dateFormat.format(new Date()),nota.getFiBanOtros());
+
+			lista = jdbcTemplate.query(query.toString(), new BeanPropertyRowMapper<NNota>(NNota.class));
 
 		} catch (Exception e) {
 
-			logger.error(" Error insert [ NNotaDAO ] ", e);
+			logger.error(" Error findByIdClassVideo [ NNotaDAO ] ", e);
 
 			throw new NNotaDAOException(e.getMessage());
 
 		}
 
-		return rows;
+		if (lista.isEmpty() || lista == null) {
+			return Collections.emptyList();
+		}
+
+		return lista;
 	}
+
 }
